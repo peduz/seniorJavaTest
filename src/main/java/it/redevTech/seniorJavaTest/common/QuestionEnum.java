484f301ddb1164c91856bd0253b881e9e9ce28ee
peduz <peduz91@gmail.com>
2022-02-18 00:44:40 +0100

    Added some errors

--- src/main/java/it/redevTech/seniorJavaTest/common/QuestionEnum.java
+++ src/main/java/it/redevTech/seniorJavaTest/common/QuestionEnum.java
@@ -3,7 +3,7 @@ package it.redevTech.seniorJavaTest.common;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum QuestionEnum {
-	SUM_TWO_VALUES_ARRAY("sumTwoValues"), SUM_SUBARRAY("sumSubarray");
+	SUM_TWO_VALUES_ARRAY("sumTwoValues");
 	
 	private String value;
 	
